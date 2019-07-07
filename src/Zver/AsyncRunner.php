649503799298ce1d8f3ab798570ab1dbfb68e834
AlexanderTarasov <k2L3q28s>
2019-07-07 13:22:50 +0300

    frist version on callbacks

--- src/Zver/AsyncRunner.php
+++ src/Zver/AsyncRunner.php
@@ -17,52 +17,81 @@ class AsyncRunner
     protected const TASK_CALLBACK = 'CALLBACK';
     protected const TASK_SUCCESS  = 'SUCCCESS';
     protected const TASK_FAIL     = 'FAIL';
+    protected const TASK_TIMEOUT  = 'TIMEOUT';
 
     protected $runnedAtTimestamp = false;
+    protected $results           = [];
+    protected $taskTimeout       = 0;
 
     protected $queue = [];
 
+    /**
+     * @var Pool $pool
+     */
     protected $pool;
 
-    protected $concurrency = 20;
+    protected $concurrency;
 
     protected $concurrencyTimeout;
 
-    public function __construct(int $taskRunPauseSeconds = 0, int $maxTaskAtSameTime = 20, int $killTaskAfterSeconds = 0)
+    public function __construct(int $taskRunPauseSeconds = 0, int $maxTaskAtSameTime = 20, int $killTaskAfterSeconds = 3600)
     {
         if (!Pool::isSupported()) {
             throw new Exception('Async run is not supported, required extensions PCNTL and POSIX');
         }
+        $this->taskTimeout = $killTaskAfterSeconds;
+        $this->concurrency = $maxTaskAtSameTime;
         $this->concurrencyTimeout = $taskRunPauseSeconds;
-        $this->pool = Pool::create()
-                          ->concurrency($maxTaskAtSameTime)
-                          ->timeout($killTaskAfterSeconds);
     }
 
-    public function addTask(callable $callback, callable $onSuccess, callable $onError)
+    protected function createPool()
+    {
+        $this->pool = Pool::create()
+                          ->concurrency($this->concurrency)
+                          ->timeout($this->taskTimeout);
+    }
+
+    public function addTask(callable $callback, callable $onSuccess, callable $onError, callable $onTimeout)
     {
         $this->queue[] = [
             static::TASK_CALLBACK => $callback,
             static::TASK_SUCCESS  => $onSuccess,
             static::TASK_FAIL     => $onError,
+            static::TASK_TIMEOUT  => $onTimeout,
         ];
         return $this;
     }
 
     protected function isQueueEmpty(): bool
     {
-        return count($this->queue) == 0;
+        foreach ($this->queue as $value) {
+            if (!is_null($value)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     protected function addTasksFromQueue()
     {
+        $this->createPool();
         foreach ($this->queue as $index => $task) {
+            if (is_null($task)) {
+                continue;
+            }
             if ($this->isTimeToRunTask($index)) {
                 $this->pool->add($task[static::TASK_CALLBACK])
                            ->then($task[static::TASK_SUCCESS])
-                           ->catch($task[static::TASK_FAIL]);
+                           ->catch($task[static::TASK_FAIL])
+                           ->timeout($task[static::TASK_TIMEOUT]);
+                $this->unsetTask($index);
             }
         }
+    }
+
+    protected function unsetTask($index)
+    {
+        $this->queue[$index] = null;
     }
 
     protected function isTimeToRunTask($taskIndex): bool
@@ -76,7 +105,7 @@ class AsyncRunner
     {
         while (!$this->isQueueEmpty()) {
             $this->addTasksFromQueue();
-            $this->pool->wait();
+            $this->results = array_merge($this->results, $this->pool->wait());
         }
     }
 
@@ -88,6 +117,7 @@ class AsyncRunner
         }
         $this->runnedAtTimestamp = time();
         $this->run();
+        return $this->results;
     }
 
 }
--- tests/AsyncRunnerTest.php
+++ tests/AsyncRunnerTest.php
@@ -27,21 +27,107 @@ class AsyncRunnerTest extends PHPUnit\Framework\TestCase
 
     public function testRunQueueWithoutTimeout()
     {
-        $callback = function () {
-            $n = rand(1, 5);
-            sleep($n);
-            echo $n;
-        };
-
-        $empty = function () {
-        };
-
         $runner = new AsyncRunner();
         for ($i = 0; $i < 10; $i++) {
-            $runner->addTask($callback, $empty, $empty);
+
+            $callback = function () use ($i) {
+                $n = rand(1, 5);
+                sleep($n);
+                return $i;
+            };
+
+            $success = function ($result) {
+                return $result;
+            };
+
+            $failed = function (Throwable $e) {
+                throw $e;
+            };
+
+            $timeout = function () use ($i) {
+
+            };
+
+            $runner->addTask($callback, $success, $failed, $timeout);
         }
-        $runner->runAndWait();
-        $this->assertTrue(true);
+        $syncExecutionResult = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        $results = $runner->runAndWait();
+        $this->assertNotEmpty($results);
+        $this->assertNotEquals($results, $syncExecutionResult);
+        $this->assertTrue(count($results) == 10);
+        foreach ($syncExecutionResult as $value) {
+            $this->assertTrue(in_array($value, $results));
+        }
+    }
+
+    public function testRunQueueWithRunTimeout()
+    {
+        $runner = new AsyncRunner(2);
+        for ($i = 0; $i < 10; $i++) {
+
+            $callback = function () use ($i) {
+                $n = rand(1, 5);
+                sleep($n);
+                return $i;
+            };
+
+            $success = function ($result) {
+                return $result;
+            };
+
+            $failed = function (Throwable $e) {
+                throw $e;
+            };
+
+            $timeout = function () use ($i) {
+
+            };
+
+            $runner->addTask($callback, $success, $failed, $timeout);
+        }
+        $syncExecutionResult = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        $results = $runner->runAndWait();
+        $this->assertNotEmpty($results);
+        $this->assertNotEquals($results, $syncExecutionResult);
+        $this->assertTrue(count($results) == 10);
+        foreach ($syncExecutionResult as $value) {
+            $this->assertTrue(in_array($value, $results));
+        }
+    }
+
+    public function testRunQueueWithRunTimeoutIsReal()
+    {
+        $runner = new AsyncRunner(10); //sync simulation to check timeout to run realy executed
+        for ($i = 0; $i < 10; $i++) {
+
+            $callback = function () use ($i) {
+                $n = rand(1, 5);
+                sleep($n);
+                return $i;
+            };
+
+            $success = function ($result) {
+                return $result;
+            };
+
+            $failed = function (Throwable $e) {
+                throw $e;
+            };
+
+            $timeout = function () use ($i) {
+
+            };
+
+            $runner->addTask($callback, $success, $failed, $timeout);
+        }
+        $syncExecutionResult = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        $results = $runner->runAndWait();
+        $this->assertNotEmpty($results);
+        $this->assertEquals($results, $syncExecutionResult);
+        $this->assertTrue(count($results) == 10);
+        foreach ($syncExecutionResult as $value) {
+            $this->assertTrue(in_array($value, $results));
+        }
     }
 
 }
